19ec0942ab592441b27de411caafb6f773360e81
yafanfan6600 <674106486@qq.com>
2025-03-23 11:07:17 +0800

    Add conditional base path for different environments

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// 判断是否是 GitHub Pages 环境
+const isGitHubPages = process.env.GITHUB_ACTIONS || false
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: isGitHubPages ? '/image-compressor/' : '/',
   plugins: [react()],
   optimizeDeps: {
     include: ['react-dropzone']
